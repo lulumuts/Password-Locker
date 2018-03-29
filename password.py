2f23ecbd9b44249a99cf20dbc0092c930fc3e6a7
Lulu <lulumutuli24@gmail.com>
2018-03-29 14:59:59 +0300

    password saved test passed

--- password.py
+++ password.py
@@ -1,10 +1,16 @@
 class Password:
-    """
+    '''
     Class that generates new passwords.
-    """
+    '''
 
     password_list = [] #Empty password list
 
     def __init__(self,random_password):
 
         self.random_password = random_password
+
+    def save_password(self):
+        '''
+        save_password method saves password objects in password_list
+        '''
+        Password.password_list.append(self)
