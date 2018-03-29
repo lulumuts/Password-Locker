375c9b8e7016df580517839966c92d0e6092b235
Lulu <lulumutuli24@gmail.com>
2018-03-29 11:11:48 +0300

    User and password classes created

---
+++ locker-test.py
@@ -0,0 +1,3 @@
+import unittest #Importing the unittest module
+from User import User #Importing the class locker
+from password import Password #Importing the class password
---
+++ locker.py
@@ -0,0 +1,12 @@
+class User:
+    """
+    Class that generate new accounts using a login and password.
+    """
+
+    account_list = [] #Empty account list
+
+class Password:
+    """
+    Class that generates new passwords.
+
+    password_list = []
